5c27b6814e1a090a36fa3441b0ffb4379debcf4b
Ananyanagaraj11 <ananyanraj6@gmail.com>
2026-02-16 21:44:42 -0500

    CORS: allow origin https://ananyanagaraj11.github.io

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ app = FastAPI()
 # ========================= CORS =========================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ananyanagaraj11.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
